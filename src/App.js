cc20609b5be3b406de40f63ca6d0e8a71a6eb938
Panagiotis Papantonakis <panpapantonakis@gmail.com>
2019-05-28 00:23:11 +0300

    Get JSON of readability indices

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       platform: window.process.platform,
       electron: window.require('electron'),
       isDev: window.require('electron-is-dev'),
-      selectedFilesPaths: []
+      selectedFilesPaths: [],
+      resultList: []
     };
   }
 
@@ -98,12 +99,37 @@ class App extends Component {
 
     script.stdout.on('data', (data) => {
       console.log(`${data}`);
+      // this.setState(this.state.resultList.push(result))
+      data = String(data);
+      if (data.startsWith('{')) {
+        try{
+          this.state.resultList.push(JSON.parse(data));
+        }
+        catch(e) {
+          if(e === SyntaxError) {
+            const multipleDataList = data.split('\n');
+            multipleDataList.forEach((json) => {
+              this.state.resultList.push(JSON.parse(json));
+            });
+          }
+        }
+      }
     });
 
 
     script.on('exit', (code) => {
+      console.log(this.state.resultList);
       console.log(`child process exited with code ${code}`);
       execButton.disabled = false;
+      this.state.resultList = [];
+    });
+  }
+
+  checkAll = () => {
+    const checkboxes = document.querySelectorAll(".read-index");
+    const isChecked = document.querySelector("#check-all").checked;
+    checkboxes.forEach((checkbox) => {
+      checkbox.checked = isChecked;
     });
   }
 
@@ -122,44 +148,50 @@ class App extends Component {
         </button>
         <div id="selected-files">
         </div>
-        <input type="checkbox" class="read-index" name="readability-index" value="ARI" />ARI
-        <input type="checkbox" class="read-index" name="readability-index" value="ARI.NRI" />(ARI NRI)
-        <input type="checkbox" class="read-index" name="readability-index" value="ARI.simple" />(ARI simplified)
-        <input type="checkbox" class="read-index" name="readability-index" value="Bormuth" />Bormuth
-        <input type="checkbox" class="read-index" name="readability-index" value="Coleman" />Coleman
-        <input type="checkbox" class="read-index" name="readability-index" value="Coleman.Liau" />Coleman.Liau
-        <input type="checkbox" class="read-index" name="readability-index" value="Dale.Chall" />Dale.Chall
-        <input type="checkbox" class="read-index" name="readability-index" value="Danielson.Bryan" />Danielson.Bryan
-        <input type="checkbox" class="read-index" name="readability-index" value="Dickes.Steiwer" />Dickes.Steiwer
-        <input type="checkbox" class="read-index" name="readability-index" value="DRP" />DRP
-        <input type="checkbox" class="read-index" name="readability-index" value="ELF" />ELF
-        <input type="checkbox" class="read-index" name="readability-index" value="Farr.Jenkins.Paterson" />Farr.Jenkins.Paterson
-        <input type="checkbox" class="read-index" name="readability-index" value="Farr.Jenkins.Paterson.PSK" />Farr.Jenkins.Paterson PSK
-        <input type="checkbox" class="read-index" name="readability-index" value="Flesch" />Flesch
-        <input type="checkbox" class="read-index" name="readability-index" value="Flesch.PSK" />Flesch PSK
-        <input type="checkbox" class="read-index" name="readability-index" value="Flesch.Kincaid" />Flesch.Kincaid
-        <input type="checkbox" class="read-index" name="readability-index" value="FOG" />FOG
-        <input type="checkbox" class="read-index" name="readability-index" value="FOG.PSK" />FOG PSK
-        <input type="checkbox" class="read-index" name="readability-index" value="FOG.NRI" />FOG NRI
-        <input type="checkbox" class="read-index" name="readability-index" value="FORCAST.RGL" />FORCAST Reading Grade Level
-        <input type="checkbox" class="read-index" name="readability-index" value="Fucks" />Fucks
-        <input type="checkbox" class="read-index" name="readability-index" value="Harris.Jacobson" />Harris.Jacobson
-        <input type="checkbox" class="read-index" name="readability-index" value="Linsear.Write" />Linsear.Write
-        <input type="checkbox" class="read-index" name="readability-index" value="LIX" />LIX
-        <input type="checkbox" class="read-index" name="readability-index" value="nWS" />nWS
-        <input type="checkbox" class="read-index" name="readability-index" value="RIX" />RIX
-        <input type="checkbox" class="read-index" name="readability-index" value="SMOG" />SMOG
-        <input type="checkbox" class="read-index" name="readability-index" value="SMOG.C" />SMOG C
-        <input type="checkbox" class="read-index" name="readability-index" value="SMOG.simple" />SMOG simplified
-        <input type="checkbox" class="read-index" name="readability-index" value="Spache" />Spache
-        <input type="checkbox" class="read-index" name="readability-index" value="Strain" />Strain
-        <input type="checkbox" class="read-index" name="readability-index" value="Traenkle.Bailer" />Traenkle.Bailer
-        <input type="checkbox" class="read-index" name="readability-index" value="TRI" />TRI
-        <input type="checkbox" class="read-index" name="readability-index" value="Tuldava" />Tuldava
-        <input type="checkbox" class="read-index" name="readability-index" value="Wheeler.Smith" />Wheeler.Smith
+        <div id="select-read-indices">
+          <input type="checkbox" id="check-all" name="readability-index" value="all" onClick={this.checkAll} />All
+          <input type="checkbox" className="read-index" name="readability-index" value="ARI" />ARI
+          <input type="checkbox" className="read-index" name="readability-index" value="ARI.NRI" />(ARI NRI)
+          <input type="checkbox" className="read-index" name="readability-index" value="ARI.simple" />(ARI simplified)
+          <input type="checkbox" className="read-index" name="readability-index" value="Bormuth" />Bormuth
+          <input type="checkbox" className="read-index" name="readability-index" value="Coleman" />Coleman
+          <input type="checkbox" className="read-index" name="readability-index" value="Coleman.Liau" />Coleman.Liau
+          <input type="checkbox" className="read-index" name="readability-index" value="Dale.Chall" />Dale.Chall
+          <input type="checkbox" className="read-index" name="readability-index" value="Danielson.Bryan" />Danielson.Bryan
+          <input type="checkbox" className="read-index" name="readability-index" value="Dickes.Steiwer" />Dickes.Steiwer
+          <input type="checkbox" className="read-index" name="readability-index" value="DRP" />DRP
+          <input type="checkbox" className="read-index" name="readability-index" value="ELF" />ELF
+          <input type="checkbox" className="read-index" name="readability-index" value="Farr.Jenkins.Paterson" />Farr.Jenkins.Paterson
+          <input type="checkbox" className="read-index" name="readability-index" value="Farr.Jenkins.Paterson.PSK" />Farr.Jenkins.Paterson PSK
+          <input type="checkbox" className="read-index" name="readability-index" value="Flesch" />Flesch
+          <input type="checkbox" className="read-index" name="readability-index" value="Flesch.PSK" />Flesch PSK
+          <input type="checkbox" className="read-index" name="readability-index" value="Flesch.Kincaid" />Flesch.Kincaid
+          <input type="checkbox" className="read-index" name="readability-index" value="FOG" />FOG
+          <input type="checkbox" className="read-index" name="readability-index" value="FOG.PSK" />FOG PSK
+          <input type="checkbox" className="read-index" name="readability-index" value="FOG.NRI" />FOG NRI
+          <input type="checkbox" className="read-index" name="readability-index" value="FORCAST" />FORCAST
+          <input type="checkbox" className="read-index" name="readability-index" value="FORCAST.RGL" />FORCAST Reading Grade Level
+          <input type="checkbox" className="read-index" name="readability-index" value="Fucks" />Fucks
+          <input type="checkbox" className="read-index" name="readability-index" value="Harris.Jacobson" />Harris.Jacobson
+          <input type="checkbox" className="read-index" name="readability-index" value="Linsear.Write" />Linsear.Write
+          <input type="checkbox" className="read-index" name="readability-index" value="LIX" />LIX
+          {/* <input type="checkbox" className="read-index" name="readability-index" value="nWS" />nWS */}
+          <input type="checkbox" className="read-index" name="readability-index" value="RIX" />RIX
+          <input type="checkbox" className="read-index" name="readability-index" value="SMOG" />SMOG
+          <input type="checkbox" className="read-index" name="readability-index" value="SMOG.C" />SMOG C
+          <input type="checkbox" className="read-index" name="readability-index" value="SMOG.simple" />SMOG simplified
+          <input type="checkbox" className="read-index" name="readability-index" value="Spache" />Spache
+          <input type="checkbox" className="read-index" name="readability-index" value="Strain" />Strain
+          <input type="checkbox" className="read-index" name="readability-index" value="Traenkle.Bailer" />Traenkle.Bailer
+          <input type="checkbox" className="read-index" name="readability-index" value="TRI" />TRI
+          <input type="checkbox" className="read-index" name="readability-index" value="Tuldava" />Tuldava
+          <input type="checkbox" className="read-index" name="readability-index" value="Wheeler.Smith" />Wheeler.Smith
+        </div>
         <button id="execute" onClick={this.executeScript}>
           Execute
         </button>
+        <div id="results">
+        </div>
       </div>
     );
   }
--- src/readability_indices.R
+++ src/readability_indices.R
@@ -16,9 +16,16 @@ for (i in 1:length(filePaths)) {
     book <- readtext(filePaths[i])
     tagged_text <- treetag(filePaths[i], treetagger="manual", lang=get.kRp.env(lang=T), TT.options=list(path="C:\\TreeTagger", preset="en"), doc_id="sample")
     hyphened_text <- hyphen(tagged_text, quiet=T)
-    tokens <- tokens(book$text, remove_punct=T, what="word")
+    # tokens <- tokens(book$text, remove_punct=T, what="word")
     dale_chall_wordlist <- readtext("src\\daleChallWordList.txt")
     dale_chall_wordlist <- tokens(dale_chall_wordlist$text, what="word")
     read_indices <- readability(tagged_text, index=index, hyphen=hyphened_text, word.lists=list(Bormuth=dale_chall_wordlist$text1, Dale.Chall=dale_chall_wordlist$text1, Harris.Jacobson=dale_chall_wordlist$text1, Spache=dale_chall_wordlist$text1))
-    print(serializeJSON(summary(read_indices)))
+    
+    indices <- list()
+    for(id in index) {
+        slot(read_indices, id)[c("word.list", "not.on.list", "FOG.hard.words", "dropped")] <- NULL
+        slot(read_indices, id)["id"] <- id
+        # indices <- append(indices, slot(read_indices, id))
+        print(toJSON(slot(read_indices, id), auto_unbox=T))
+    }
 }
