594d1ecc4c4473c9d6abd4e5b76d97a3bd74c74f
Panagiotis Papantonakis <panpapantonakis@gmail.com>
2019-06-07 16:48:09 +0300

    Fix output format and output multiple results to table

--- src/Built-in/readability/readability_indices.R
+++ src/Built-in/readability/readability_indices.R
@@ -26,30 +26,35 @@ for(arg in args) {
     }
     temp <- c(temp, arg);
 }
-
-resultFilePath <- "results.json"
 index <- temp;
-write("[", file=resultFilePath)
+
+fileNames <- basename(filePaths)
+resultFilePath <- "results.json"
+
+dale_chall_wordlist <- readtext("src\\Built-in\\readability\\daleChallWordList.txt")
+dale_chall_wordlist <- tokens(dale_chall_wordlist$text, what="word")
+
+resultList <- c()
 for (i in 1:length(filePaths)) {
+    resultObj = list()
+    resultObj["name"] <- fileNames[i]
     # book <- readtext(filePaths[i])
     tagged_text <- treetag(filePaths[i], treetagger="manual", lang=get.kRp.env(lang=T), TT.options=list(path="C:\\TreeTagger", preset="en"), doc_id="sample")
     hyphened_text <- hyphen(tagged_text, quiet=T)
     # tokens <- tokens(book$text, remove_punct=T, what="word")
     
     # dale_chall_wordlist is a list of 3000 common english words, needed for some readability indices
-    dale_chall_wordlist <- readtext("src\\Built-in\\readability\\daleChallWordList.txt")
-    dale_chall_wordlist <- tokens(dale_chall_wordlist$text, what="word")
     read_indices <- readability(tagged_text, index=index, hyphen=hyphened_text, word.lists=list(Bormuth=dale_chall_wordlist$text1, Dale.Chall=dale_chall_wordlist$text1, Harris.Jacobson=dale_chall_wordlist$text1, Spache=dale_chall_wordlist$text1))
     
-    indices <- list()
     result <- c()
     for(id in index) {
         # Delete unwanted fields of data frames
         slot(read_indices, id)[c("word.list", "not.on.list", "FOG.hard.words", "dropped")] <- NULL
         slot(read_indices, id)["id"] <- id
-        # indices <- append(indices, slot(read_indices, id))
-        result <- c(result, toJSON(slot(read_indices, id), auto_unbox=T))
+        result <- c(result, list(slot(read_indices, id)))
     }
+    resultObj[["readIndices"]] <- result
+    resultList <- c(resultList, list(resultObj))
 }
-write(result, file=resultFilePath, append=T, sep=',', ncolumns=length(result))
-write("]", file=resultFilePath, append=T)
+
+write(toJSON(resultList), file=resultFilePath, sep=',')
--- src/ResultsTab.js
+++ src/ResultsTab.js
@@ -4,21 +4,49 @@ import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import "react-tabs/style/react-tabs.css";
 
 const ResultsTab = props => {
+    console.log(props.resultList);
+
+    const header = (() => {
+        try {
+            return (<tr>
+                <th>FileName</th>
+                {props.resultList[0].readIndices.map((resultObj, i) =>
+                    <th key={i}>{resultObj.id}</th>
+                )}
+            </tr>)
+        }
+        catch (e) {
+            return (
+                <tr>
+                    <div>No results</div>
+                </tr>)
+        }
+    })()
+    
+    const body = (() => {
+        try {
+            return (props.resultList.map((resultObj, i) =>
+            <tr key={i}>
+                <td>{resultObj.name[0]}</td>
+                {resultObj.readIndices.map((readIndicesObj, i) =>
+                    <td key={i}>{readIndicesObj.grade}</td>
+                )}
+                </tr>))
+        }
+        catch (e) {
+            return (
+                <tr>
+                </tr>)
+        }
+    })()
+
     const resultTable =
         <table id="results">
             <thead>
-                <tr>
-                    {props.resultList.map((resultObj, i) =>
-                        <th key={i}>{resultObj.id}</th>
-                    )}
-                </tr>
+                {header}
             </thead>
             <tbody>
-                <tr>
-                    {props.resultList.map( (resultObj,i) =>
-                        <td key={i}>{resultObj.grade}</td>
-                    )}
-                </tr>
+                {body}
             </tbody>
 
         </table>
