f8ea8106836e6a7bcd7e21c56f3bacff079f1288
Panagiotis Papantonakis <panpapantonakis@gmail.com>
2019-06-23 15:16:25 +0300

    Adapt results tab

--- src/ResultsTab.js
+++ src/ResultsTab.js
@@ -1,89 +1,108 @@
 import React, { Component } from 'react';
+import clsx from 'clsx';
 import './ResultsTab.css';
+import { withStyles } from '@material-ui/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+const styles = theme => ({
+
+    table: {
+        width: "100%",
+        margin: "0 10px 0 10px"
+    },
+
+});
+
 const ResultsTab = props => {
     // pass by value
     // TODO : find better way?
     let resultList = [];
     console.log(props.resultList)
     props.resultList.forEach(elem => {
-        resultList.push(Object.assign([], elem)); 
+        resultList.push(Object.assign([], elem));
     });
 
     resultList.forEach(bookObj => {
         const indices = bookObj.indices;
         delete bookObj.indices;
         Object.keys(indices).map(index => {
-            if(indices[index].length !== 0)
-            bookObj[index] = indices[index];
+            if (indices[index].length !== 0)
+                bookObj[index] = indices[index];
         });
         return bookObj
     });
+
+    const classes = props.classes;
+
     return (
         <div>
             <button id="execute" onClick={props.executeAll}>Execute</button>
-            <table className="resultsTable">
+            <Table className={clsx(classes.table)}>
                 {(() => {
                     try {
-                        return (<thead>
-                            <tr>
+                        return (<TableHead>
+                            <TableRow>
                                 {Object.keys(resultList[0]).map((indexName, i) => {
-                                    if (typeof(resultList[0][indexName]) !== typeof({}) || Array.isArray(resultList[0][indexName])) {
-                                        return <th rowSpan="2" key={i}>{indexName}</th>
+                                    if (typeof (resultList[0][indexName]) !== typeof ({}) || Array.isArray(resultList[0][indexName])) {
+                                        return <TableCell rowSpan="2" key={i}>{indexName}</TableCell>
                                     }
-                                    return <th colSpan={Object.keys(Object.keys(resultList[0][indexName])).length} key={i}>{indexName}</th>
-                                    
+                                    return <TableCell colSpan={Object.keys(Object.keys(resultList[0][indexName])).length} key={i}>{indexName}</TableCell>
+
                                 })}
-                            </tr>
-                            <tr>
+                            </TableRow>
+                            <TableRow>
                                 {Object.keys(resultList[0]).map((indexName, i) => {
-                                    if (typeof(resultList[0][indexName]) !== typeof({}) || Array.isArray(resultList[0][indexName]))
-                                    return;
-                                        return Object.keys(resultList[0][indexName]).map((el, idx) =><th key={idx}>{el}</th>
-                                        )
+                                    if (typeof (resultList[0][indexName]) !== typeof ({}) || Array.isArray(resultList[0][indexName]))
+                                        return;
+                                    return Object.keys(resultList[0][indexName]).map((el, idx) => <TableCell key={idx}>{el}</TableCell>
+                                    )
                                 })}
-                            </tr>
-                        </thead>
+                            </TableRow>
+                        </TableHead>
                         )
                     }
                     catch (e) {
                         return (
-                            <thead>
-                                <tr>
-                                    <td>No results</td>
-                                </tr>
-                            </thead>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>No results</TableCell>
+                                </TableRow>
+                            </TableHead>
                         )
                     }
                 })()}
-                <tbody>
+                <TableBody>
                     {(() => {
                         try {
                             return resultList.map((elem, i) =>
-                                <tr key={i}>
+                                <TableRow key={i}>
                                     {Object.values(elem).map((value, id) => {
                                         if (typeof (value) === typeof ({})) {
                                             return Object.values(value).map((val, i) =>
-                                                <td key={i}>{val}</td>
+                                                <TableCell key={i}>{val}</TableCell>
                                             )
                                         }
                                         else {
-                                            return <td key={id}>{value}</td>
+                                            return <TableCell key={id}>{value}</TableCell>
                                         }
                                     })
                                     }
-                                </tr>)
+                                </TableRow>)
                         }
                         catch (e) {
                             return (
-                                <tr>
-                                </tr>)
+                                <TableRow>
+                                </TableRow>)
                         }
                     })()}
-                </tbody>
-            </table>
-        </div>
+                </TableBody>
+            </Table>
+        </div >
     );
 };
 
-export default ResultsTab;
-
+export default withStyles(styles)(ResultsTab);
