b2db8b3208c71c98ee3fa3ef45ad4f93f72f5cbb
Panagiotis Papantonakis <panpapantonakis@gmail.com>
2019-07-18 13:00:33 +0300

    Scroll to bottom of the console

--- src/Console.js
+++ src/Console.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/styles';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 
 const styles = theme => ({
 
     footer: {
         position: 'absolute',
         bottom: '0',
+        left: '0',
+        right: '0',
         width: '100%',
-        height: '250px',
-        overflow: 'scroll'
+        height: '20%',
+        overflow: 'scroll',
     },
 
 });
@@ -33,22 +36,23 @@ class Console extends Component {
                 logArray: [...this.state.logArray, {
                     dateInstance: date,
                     message: logMessage
-                }]
+                }],
+            }, () => {
+                const consoleDiv = document.getElementById("console");
+                consoleDiv.scrollTop = consoleDiv.scrollHeight;
             })
         }
     }
 
-
-
     render() {
         const classes = this.props.classes;
         return (
-            <div className={classes.footer}>
+            <Paper id="console" classes={{ root: classes.footer }}>
                 {this.state.logArray.map((logObj, id) => (
                     <Typography key={id}>{logObj.message}</Typography>
-                )
-                )}
-            </div>);
+                ))}
+            </Paper>
+        )
     }
 }
 
