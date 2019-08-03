b17f5f028dd7bbe81556d17edd3a3efa69bac09e
Panagiotis Papantonakis <panpapantonakis@gmail.com>
2019-08-03 19:54:58 +0300

    Create custom theme for app styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,38 @@ import Settings from './Settings';
 import { Switch, Route } from 'react-router-dom';
 import "react-tabs/style/react-tabs.css";
 
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const palette = {
+  primary: { main: '#1B5E20', light: '#4C8C4A', dark: '#003300' },
+  secondary: { main: '#FFAB40', light: '#FFDD71', dark: '#C77C02' }
+};
+
+const themeName = 'Custom theme';
+
+const theme = createMuiTheme({
+  palette,
+  themeName,
+  overrides:
+  {
+    MuiListItem: {
+      "root": {
+        "&$selected": {
+          "background-color": palette.primary.light,
+        },
+      }
+    },
+    MuiTab: {
+      "textColorInherit": {
+        "&$selected": {
+          "background-color": palette.secondary.main,
+        },
+      }
+    }
+  }
+});
+
 /* App is the main component which renders all
 * application routes which relate to different application windows
 */
@@ -20,10 +52,12 @@ class App extends Component {
   }
   render() {
     return (
-      <Switch>
-        <Route path='/settings' render={() => <Settings platform={this.state.platform} electron={this.state.electron} isDev={this.state.isDev} />} />
-        <Route path='/main' render={() => <Main platform={this.state.platform} electron={this.state.electron} isDev={this.state.isDev} />} />
-      </Switch>
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route path='/settings' render={() => <Settings platform={this.state.platform} electron={this.state.electron} isDev={this.state.isDev} />} />
+          <Route path='/main' render={() => <Main platform={this.state.platform} electron={this.state.electron} isDev={this.state.isDev} />} />
+        </Switch>
+      </ThemeProvider>
     );
   }
 }
